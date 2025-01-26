fbf70e2eb955feb6361c4a830edf8e1ae5f3344d
AmiroKD <2051419987@qq.com>
2025-01-26 14:42:30 +0800

    Site updated: 2025-01-26 14:42:30

--- js/fomal.js
+++ js/fomal.js
@@ -1113,7 +1113,7 @@ function changeMouseMode() {
 var now1 = new Date();
 
 function createtime1() {
-  var grt = new Date("24/01/2025 00:00:00"); //此处修改你的建站时间或者网站上线时间
+  var grt = new Date("2025/1/24 00:00:00"); //此处修改你的建站时间或者网站上线时间
   now1.setTime(now1.getTime() + 250);
   var days = (now1 - grt) / 1000 / 60 / 60 / 24;
   var dnum = Math.floor(days);
